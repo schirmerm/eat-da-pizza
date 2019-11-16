7662c0036d073e2cb616ba023c4a954f697b008b
Maverick <maverick.schirmer@gmail.com>
2019-11-15 21:05:02 -0800

    changed database

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = require('mysql2')
 .createConnection({
     host: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'qdp9pqjns3vr7dtx',
-    password: 'd5asis85i2k3ub6r',
-    database: 'oyjyyubet1171q67'
+    user: 'l0bj0hiz4itj8452',
+    password: '	bpn3xu18jvxn9gch',
+    database: 'ih8nkt9if74i7ivi'
 })
