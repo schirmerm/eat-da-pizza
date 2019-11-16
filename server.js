19d68bc0e59fcaeaf76f7764143e355c8cfa750a
Maverick <maverick.schirmer@gmail.com>
2019-11-15 20:41:56 -0800

    changed join

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { join } = require('path')
+const { resolve } = require('path')
 
 const app = express()
 
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(resolve(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.engine('.hbs', require('express-handlebars')({extname: '.hbs'}))
