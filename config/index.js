ad5a99708fffe022be0fb6b113d50624fa6b96dd
Maverick <maverick.schirmer@gmail.com>
2019-11-15 19:36:30 -0800

    changed connection to heroku

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = require('mysql2')
 .createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'pizza_db'
+    host: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'qdp9pqjns3vr7dtx',
+    password: 'd5asis85i2k3ub6r',
+    database: 'oyjyyubet1171q67'
 })
